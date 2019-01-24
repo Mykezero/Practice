14c0f5b0e3fa9848bf3254431c888fd84668a6f5
Mykezero <Mykezero@users.noreply.github.com>
2019-01-24 02:03:19 -0500

    Add template class to use as base when creating other test files

---
+++ tests/Template.cpp
@@ -0,0 +1,15 @@
+#include <CppUTest/TestHarness.h>
+
+extern "C"
+{
+    // Add includes here.
+}
+
+TEST_GROUP(Template)
+{
+};
+
+TEST(Template, FirstTest)
+{
+    CHECK_TRUE_TEXT(false, "This test is incomplete.");
+}
